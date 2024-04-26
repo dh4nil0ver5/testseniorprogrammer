a6a06b643f21114b1ece2c16d03fd6562e000c10
Nur Chamdani <kokme.km@gmail.com>
2024-04-26 23:33:31 +0700

    add new files

--- 3/star.py
+++ 3/star.py
@@ -1,25 +1,11 @@
-rows = 6
-i = 1
+r =int(input("Enter the number of rows:"))
 
-while i <= rows:
-    # Print numbers for the left side of the triangle
-    j = 1
-    while j <= rows + 1 - i:
-        print(i, end=' ')
-        j += 1
-    
-    # Print spaces in the middle
-    space = 1
-    while space <= 2 * (i - 2):
-        print(' ', end=' ')
-        space += 1
-    
-    # Print numbers for the right side of the triangle
-    if i != 1:
-        j = 1
-        while j <= rows + 1 - i:
-            print(i, end=' ')
-            j += 1
-    
-    print('')
-    i += 1
+for i in range(r,0,-1):
+    print("*"*i,end='')
+    print(" "*(r-i)*2,end='')
+    print("*"*i)
+
+for i in range(1,r+1):
+    print("*"*i,end='')
+    print(" "*(r-i)*2,end='')
+    print("*"*i)
