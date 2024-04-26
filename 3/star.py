497a52ab596260a242feb82039bec7ff5d7a0d13
Nur Chamdani <kokme.km@gmail.com>
2024-04-26 23:35:56 +0700

    add new files

--- 3/star.py
+++ 3/star.py
@@ -1,11 +1,11 @@
 r =int(input("Enter the number of rows:"))
 
 for i in range(r,0,-1):
-    print("*"*i,end='')
+    print("0"*i,end='')
     print(" "*(r-i)*2,end='')
-    print("*"*i)
+    print("0"*i)
 
 for i in range(1,r+1):
-    print("*"*i,end='')
+    print("0"*i,end='')
     print(" "*(r-i)*2,end='')
-    print("*"*i)
+    print("0"*i)
